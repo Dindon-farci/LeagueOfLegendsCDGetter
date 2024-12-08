db7920eb89eb04844fc21a412bedb74299101645
Dindin <aylahjomri@yahoo.com>
2024-12-08 12:28:23 +0100

    new file:   cooldownGetter.py     Added the Python script

---
+++ cooldownGetter.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+patchId = "14.3.1"
+
+
+def getSpellKey(i):
+    if i < 4:
+        return ("Q","W","E","R")[i]
+    else:
+        raise ValueError("More than 4 abilities")
+    
+
+def getCooldowns(championId):
+    response = requests.get("https://ddragon.leagueoflegends.com/cdn/"+ patchId +"/data/en_US/champion/"+championId+".json")
+    data = response.json()["data"][championId]
+    championName = data["name"]
+    output = f"{championName}'s cooldowns : \n"
+    i = 0
+    for spell in data["spells"]:
+        print(type(spell))
+        output += f"[{getSpellKey(i)}] {spell["name"]} cooldown: {spell["cooldown"]}\n"
+        i+=1
+    return output
